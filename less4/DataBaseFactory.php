9f54f6015412ed48c0d081720033e88a8d1312ab
Alex <azv99@yandex.ru>
2022-02-01 13:26:08 +0300

    less4 add clien code to DataBaseFactory

--- less4/DataBaseFactory.php
+++ less4/DataBaseFactory.php
@@ -6,3 +6,20 @@ abstract class DataBaseFactory {
 }
 
 
+/**
+ * Клиентский код
+*/
+
+function MySqlDataBase (DataBaseFactory $dataBaseFactory)
+{
+    $connection = $dataBaseFactory->getDBConnection();
+    $record = $dataBaseFactory->getDBRecord();
+    $querybuilder = $dataBaseFactory->getDBQueryBuilder();
+
+    echo $connection->getConnect();
+    echo $record->getRecord();
+    echo $querybuilder->getQueryBuilder();
+}
+
+
+ MySqlDataBase(new MySqlFactory());
